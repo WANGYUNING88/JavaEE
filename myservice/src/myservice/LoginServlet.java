74a39e3f58df0a7969f384cd7f11af1bbd8d6c6e
WANGYUNING <247197442@qq.com>
2018-04-29 08:23:48 +0800

    Signed-off-by: WANGYUNING <247197442@qq.com>

---
+++ myservice/src/myservice/LoginServlet.java
@@ -0,0 +1,50 @@
+package myservice;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		if(name!=null&&password!=null) {
+			if(name.equals("wang")&&password.equals("123456")) {
+				response.getWriter().append("³É¹¦");
+			}else
+				response.getWriter().append("Ê§°Ü1");
+		}else {
+			response.getWriter().append("Ê§°Ü2");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
